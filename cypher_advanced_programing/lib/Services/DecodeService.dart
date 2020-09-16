043bd7867034fbfa160c9a2022a005140c172849
Ethan Ivanier <ethan.ivanier@datacom.co.nz>
2020-09-17 09:02:28 +1200

    Finished Encoding Function

--- cypher_advanced_programing/lib/Services/DecodeService.dart
+++ cypher_advanced_programing/lib/Services/DecodeService.dart
@@ -4,12 +4,24 @@ class DecodeService {
   String decode(String key, String text) {
     int _key = int.parse(key); //Decode String of key to int
     String outText = ''; // create output text
+    int uTFoffset;
     for (int i = 0; i < text.length; i++) {
       //lop through each char in string
       int _decimalUTF = text.codeUnitAt(i); //encode string to UTF16
       if (_decimalUTF < 91 && _decimalUTF > 40) {
+        // is Capital
+        uTFoffset = 65;
       } else if (_decimalUTF < 123 && _decimalUTF > 96) {
+        // is Lowercase
+        uTFoffset = 97;
+      } else if (_decimalUTF == 32) {
+        outText += " ";
       } else {}
+
+      int countOfcharCycles = (uTFoffset + _key - uTFoffset) % 26;
+
+      outText += String.fromCharCode(countOfcharCycles + _key);
     }
+    return outText;
   }
 }
