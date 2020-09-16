333b7f5e606d65b84654b25544c50d3dceb1ded2
Ethan Ivanier <ethan.ivanier@datacom.co.nz>
2020-09-16 17:52:04 +1200

    Added home screen view

---
+++ cypher_advanced_programing/lib/SharedUI/HomeScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Container(
+            width: 300,
+            child: Text(
+                "This is a encryption and decryprion program you use this program you will need a encryption key"),
+          ),
+        ),
+        RaisedButton(
+          onPressed: null,
+          child: Text("Start"),
+        )
+      ],
+    );
+  }
+}
